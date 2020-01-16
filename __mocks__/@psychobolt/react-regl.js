f5870e71b31336fc6dfe6d580890e0ff35ae55e1
psychobolt <psychobolt@gmail.com>
2020-01-15 22:11:48 -0800

    Upgrade packages

--- __mocks__/@psychobolt/react-regl.js
+++ __mocks__/@psychobolt/react-regl.js
@@ -3,8 +3,12 @@ import React from 'react';
 const gl = jest.requireActual('gl');
 const { ReglContainer, ...rest } = jest.requireActual('@psychobolt/react-regl');
 
+// workaround for https://github.com/stackgl/headless-gl/issues/184
+const ctx = gl(1, 1, { preserveDrawingBuffer: true });
+Object.defineProperty(ctx.constructor, 'name', { value: 'WebGLRenderingContext' });
+
 const ReglContainerMock = props => (
-  <ReglContainer {...props} View={gl(1, 1, { preserveDrawingBuffer: true })} />
+  <ReglContainer {...props} View={ctx} />
 );
 
 module.exports = {
